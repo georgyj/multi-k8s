15ffd2068bb75115d738207470c9856c4b4e64f1
= <georgyj319@gmail.com>
2020-02-13 21:59:47 -0600

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t georgyjohn/multi-client:latest -t georgyjohn/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t georgyjohn/multi-server:latest -t georgyjohn/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t georgyjohn/multi-worker:latest -t georgyjohn/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push georgyjohn/multi-client:latest
+docker push georgyjohn/multi-server:latest
+docker push georgyjohn/multi-worker:latest
+docker push georgyjohn/multi-client:$SHA
+docker push georgyjohn/multi-server:$SHA
+docker push georgyjohn/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=georgyjohn/multi-server:$SHA
+kubectl set image deployment/client-deployment client=stephengrider/multi-client:latest
+kubectl set image deployment/worker-deployment worker=georgyjohn/multi-worker:$SHA
